a167af0b5402ad3e679b64529c7011572178b387
14Bme053 <ngowtham1296@gmail.com>
2019-07-24 15:13:55 -0500

    feature update: added DRV 8871 control and libraries implementation

--- visual_touch.ino/visual_touch.ino
+++ visual_touch.ino/visual_touch.ino
@@ -1,58 +1,53 @@
+#define MOTOR_IN1 10
+#define MOTOR_IN2 11
 #include <Wire.h>
 #include <VL53L0X.h>
 
 VL53L0X sensor;  //for laser distance sensor CJVL 53L0X v2
-
-int ENA = 5;             
-int IN1= 7;
-int IN2= 8;
 int Clock = A5;
 int Data = A4;
 int distance;
-int test;
+int PWM;
 
 void setup() {
-  analogWrite(6,20);
-  
-  pinMode (IN1,OUTPUT);
-  pinMode (IN2, OUTPUT);
-  pinMode (ENA, OUTPUT) ;
-  Serial.begin (9600);
-  Wire.begin();
+  Serial.begin(9600);
+  while (!Serial){
+      delay(10);
+    };
+  Serial.println("DRV8871 test");
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
   pinMode (Data, INPUT);
   pinMode (Clock, INPUT);
+  Wire.begin();
   sensor.init();
   sensor.setTimeout(1000);
 }
 
-void tof() {
-  
+void MOTOR() {
+ 
+ digitalWrite(MOTOR_IN1, LOW);
+ analogWrite(MOTOR_IN2, PWM);
+}
+
+void loop(){
   distance = (sensor.readRangeSingleMillimeters());
   if (distance <2000){
   int PWM=distance;
   PWM= map (PWM,0,1300,75,0);     // mapping the distance sensor values into PWM signals using Map function
+  digitalWrite(MOTOR_IN2, LOW);
+  analogWrite(MOTOR_IN1, PWM);
   Serial.print ("distance=");Serial.print(distance); Serial.print("; PWM=");Serial.print(PWM);
   Serial.println(); 
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2,LOW);
-  analogWrite(ENA, PWM);
+
   
   } else {
     int PWM=35;
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2,LOW);
-    analogWrite(ENA,PWM);
+    digitalWrite(MOTOR_IN2, LOW);
+    analogWrite(MOTOR_IN1, PWM);
     Serial.print(" distance=Range cannot be detected");  Serial.print("; PWM="); Serial.print(PWM);
     Serial.println();
    
   }
-  
 }
-
-void loop() {
-  //millis();
-   
-  tof();
-  
-  //Serial.print(millis()/1000);
-}
+ 
